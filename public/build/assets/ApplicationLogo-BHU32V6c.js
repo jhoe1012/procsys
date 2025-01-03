790af434cc6b2539e86fc5d85dd23c5b42986347
jhoe1012 <jhoecabornay@gmail.com>
2025-01-03 14:19:35 +0800

    - BUILD FOR TESTING

---
+++ public/build/assets/ApplicationLogo-BHU32V6c.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-9BfIP8Yt.js";function g({yellowLogo:s,className:i="",...t}){return o.jsx(o.Fragment,{children:s?o.jsx("img",{width:150,height:100,src:"/asset/logo/Goldilocks-logo-yellow.png"}):o.jsx("img",{width:150,height:100,src:"/asset/logo/Goldilocks-logo.png"})})}export{g as A};
