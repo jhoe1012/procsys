2011b86ff5f9eea57f3474e221aea5a6ce202a12
Medwin Adan <medwin.adan@gmail.com>
2025-07-31 16:26:44 +0800

    Integrated DAST

--- run_soos.cmd
+++ run_soos.cmd
@@ -1,25 +1,62 @@
 @echo off
-set "PROJECT_NAME=SCM LITE"
-set "BRANCH_NAME=dev"
-set "CLIENT_ID=qn0fh9ybn"
-set "API_KEY=ODA0ZWU1M2ItNzAzOS00ODhlLWI3N2YtZTZiYmQ4NWUzNmZj"
-set "SOURCE_CODE_PATH=./"
-set "SEMGREP_CONFIG=%CD%\semgrep-config.yml"
-@REM set "MAX_TARGET_BYTES=10000000"
+setlocal EnableDelayedExpansion
 
-REM Run Semgrep scan with custom rules and save SARIF result
-docker run --rm -v "%CD%:/src" -v "%SEMGREP_CONFIG%:/semgrep-config.yml" semgrep/semgrep semgrep --config=/semgrep-config.yml --sarif --output=/src/semgrep-results.sarif.json /src
-
-REM Install SOOS SAST if needed
-if not exist ".\soos\node_modules\@soos-io\soos-sast\bin\index.js" (
-    echo Installing SOOS SAST...
-    npm install --prefix .\soos @soos-io/soos-sast
+REM ======================================
+REM Load soos.env file
+REM ======================================
+for /f "usebackq tokens=1,* delims==" %%A in (`findstr /v "#" "soos.env"`) do (
+    if not "%%A"=="" (
+        call set "%%A=%%B"
+    )
 )
 
-REM Run SOOS SAST scan
+REM ======================================
+REM Step 1: Show Semgrep version
+REM ======================================
+echo Checking Semgrep Version
+docker run --rm semgrep/semgrep semgrep --version
+
+REM ======================================
+REM Step 2: Run Semgrep scan
+REM ======================================
+echo Running Semgrep scan
+docker run --rm -v %CD%:/src semgrep/semgrep semgrep scan ^
+  --verbose --metrics=off ^
+  --config=p/default ^
+  --config=p/owasp-top-ten ^
+  --config=p/cwe-top-25 ^
+  --sarif --sarif-output=/src/semgrep.sarif.json /src
+
+REM ======================================
+REM Step 3: Upload SARIF to SOOS
+REM ======================================
+echo Uploading SARIF to SOOS
 node .\soos\node_modules\@soos-io\soos-sast\bin\index.js ^
   --clientId=%CLIENT_ID% ^
   --apiKey=%API_KEY% ^
   --projectName="%PROJECT_NAME%" ^
   --sourceCodePath=%SOURCE_CODE_PATH% ^
   --branchName="%BRANCH_NAME%"
+
+REM ======================================
+REM Step 4: Run SOOS SCA scan
+REM ======================================
+echo Running SOOS SCA scan
+node .\soos\node_modules\@soos-io\soos-sca\bin\index.js ^
+  --clientId=%CLIENT_ID% ^
+  --apiKey=%API_KEY% ^
+  --projectName="%PROJECT_NAME%" ^
+  --branchName="%BRANCH_NAME%"
+
+
+REM ======================================
+REM Step 5: Run SOOS DAST (Container Image Scan)
+REM ======================================
+echo Running SOOS DAST container scan
+docker run -it soosio/csa ^
+  --clientId=%CLIENT_ID% ^
+  --apiKey=%API_KEY% ^
+  --projectName="%PROJECT_NAME%" ^
+  --targetUrl=http://host.docker.internal:80
+
+endlocal
